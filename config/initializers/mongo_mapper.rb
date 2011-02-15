f3608a395641a258494a76dd1eab9cbf5859cac2
Sean Grove <otokorasii@gmail.com>
2011-02-14 21:49:54 -0800

    Bushidofy mongomapper

--- config/initializers/mongo_mapper.rb
+++ config/initializers/mongo_mapper.rb
@@ -1,6 +1,13 @@
 logger = Logger.new('log/development.log')
-MongoMapper.connection = Mongo::Connection.new('127.0.0.1', 27017, :logger => logger)
-MongoMapper.database = 'thundersurvey'
+
+if ENV['MONGODB_URL'] 
+    MongoMapper.config = {RAILS_ENV => {'uri' => ENV['MONGODB_URL']}}
+    MongoMapper.connect(RAILS_ENV)
+  else
+    MongoMapper.connection = Mongo::Connection.new('127.0.0.1', 27017, :logger => logger)
+    MongoMapper.database = ENV['MONGDO_DB']
+end
+
 
 if defined?(PhusionPassenger)
    PhusionPassenger.on_event(:starting_worker_process) do |forked|
