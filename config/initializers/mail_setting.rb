9f3882eb44fed9d94329662a472efb79bca4f52c
Sean Grove <otokorasii@gmail.com>
2011-02-14 21:52:42 -0800

    Let's use bushido's smtp server. It's easier, ffs.

--- config/initializers/mail_setting.rb
+++ config/initializers/mail_setting.rb
@@ -1,11 +1,11 @@
 ActionMailer::Base.smtp_settings = {
-  :address              => "thundersurvey.com",
-  :port                 => 25,
-  :domain               => "thundersurvey.com",
-  :user_name            => "noreply@thundersurvey.com",
-  :password             => "changeme",
-  :authentication       => "login",
-  :enable_starttls_auto => true
+  :address              => ENV['SMTP_SERVER'],
+  :port                 => ENV['SMTP_PORT'],
+  :domain               => ENV['SMTP_DOMAIN'],
+  :user_name            => ENV['SMTP_USER'],
+  :password             => ENV['SMTP_PASS'],
+  :authentication       => ENV['SMTP_AUTHENTICATION'],
+  :enable_starttls_auto => ENV['SMTP_TLS']
 } 
 
 ActionMailer::Base.default_url_options[:host] = (Rails.env == 'production' ? "www.thundersurvey.com" : "localhost")
